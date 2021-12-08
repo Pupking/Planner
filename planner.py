75b0edc4dd9650af8bca0fad5ece743b754c2a74
Pupking <dsh17082001@gmail.com>
2021-12-09 02:54:13 +0530

    added login and register in backend

--- planner.py
+++ planner.py
@@ -1,6 +1,6 @@
 from Backend import app, db
-from DBHandler.models import User
+from DBHandler.models import User, Task
 
 @app.shell_context_processor
 def make_shell_context():
-    return{'db': db, 'User': User}
+    return{'db': db, 'User': User, 'Task': Task}
