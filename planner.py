89b4777268e8caff5e1990e6a725fa06e49ae6bb
Pupking <dsh17082001@gmail.com>
2021-12-08 21:40:40 +0530

    connect backend and dbhandler(sqlalchemy) create db, and migrate

--- planner.py
+++ planner.py
@@ -1,1 +1,6 @@
-from Backend import app
+from Backend import app, db
+from DBHandler.models import User
+
+@app.shell_context_processor
+def make_shell_context():
+    return{'db': db, 'User': User}
